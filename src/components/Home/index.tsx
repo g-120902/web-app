c9ad107ce70fde5782bf0a433fa526e309ee1931
g-120902 <gregoryalbert1209@gmail.com>
2024-06-20 22:10:01 +0400

    trying hard

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -12,7 +12,7 @@ export default function Play(): JSX.Element {
     const [userTokenJson, setUserTokenJson] = useState<Record<string, any> | null>(null);
     const [email, setEmail] = useState("");
     const t = useTranslations("home");
-    
+
     useEffect(() => {
         if (CheckLogin()) {
             const userTokenString = localStorage.getItem('userInfo');
@@ -31,26 +31,39 @@ export default function Play(): JSX.Element {
             setUserTokenJson(null);
         }
     }, [isLoggedIn]);
- 
+
     useEffect(() => {
         setEmail(userTokenJson?.firstname || "");
     }, [userTokenJson]);
 
     return (
         <section
-            className="bg-cover bg-center h-screen brightness-59 py-40"
+            className="bg-cover bg-center h-screen brightness-59 py-20 overflow-hidden"
             style={{ backgroundImage: `url('/assets/images/home-background.jpg')` }}
         >
             <div className="flex flex-col text-center gap-5 h-full">
-                <div className="h-50"/>
-                <div className="flex justify-center mb-3">
-                    <Image 
-                        src="/assets/images/main.png" 
-                        alt="Main Logo"  
-                        width={400} 
-                        height={400} 
+                <div className="h-50" />
+                <div className="grid grid-cols-3 w-full justify-center">
+                    <span />
+                    <div className="flex justify-center items-center">
+
+                        <Image
+                            src="/assets/images/main.png"
+                            alt="Main Logo"
+                            width={400}
+                            height={400}
+                        />
+                    </div>
+
+                    <Image
+                        src="/assets/images/content/home-knight.gif"
+                        alt="Knight"
+                        width={300}
+                        height={300}
                     />
+
                 </div>
+
                 {/* <div className="md:text-7xl text-3xl text-white uppercase select-none font-primary-bold">{t("title")}</div> */}
                 <div className={`${isLoggedIn ? `` : `hidden`}`}>
                     <UserBox email={email} />
